41ea2f0fcaf684b1d12b4a72232da90bd2023a90
Akshay <40710016+akshaykalucha3@users.noreply.github.com>
2021-08-21 19:36:50 +0530

    update

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,16 +13,16 @@ class Contact extends Component {
                     <p className="contactmehead">Or fill in the contact form down below</p>
                     <form action="#" className="contactForm">
                         <div className="contactformel">
-                            <label for="name" aria-label="please insert your name">Full name:<input name="name" type="text" id="name" error="0" className="formfield" value="" /></label>
+                            <label htmlFor="name" aria-label="please insert your name">Full name:<input name="name" type="text" id="name" error="0" className="formfield" value="" /></label>
                         </div>
                         <div className="contactformel">
-                            <label for="email" aria-label="please insert your email">Your email:<input name="email" type="email" id="email" error="0" className="formfield" value="" /></label>
+                            <label htmlFor="email" aria-label="please insert your email">Your email:<input name="email" type="email" id="email" error="0" className="formfield" value="" /></label>
                         </div>
                         <div className="contactformel">
-                            <label for="message" aria-label="please insert your message">Message:<textarea name="message" id="message" error="0" textarea="true" className="formtextarea"></textarea></label>
+                            <label htmlFor="message" aria-label="please insert your message">Message:<textarea name="message" id="message" error="0" textarea="true" className="formtextarea"></textarea></label>
                         </div>
                         <div className="contactformbtn">
-                            <button type="submit" className="formsendbtn">Send</button>
+                            <button htmlFor="submit" className="formsendbtn">Send</button>
                         </div>
                     </form>
                 </div>
