3d9b53a92f5050ac79aa140d6d9fa3e347c3fa40
Akshay <40710016+akshaykalucha3@users.noreply.github.com>
2021-08-23 17:46:25 +0530

    added functionality to footer card

--- src/components/Base/FooterCard.jsx
+++ src/components/Base/FooterCard.jsx
@@ -4,7 +4,24 @@ import './Footercard.css';
 class FooterCard extends Component {
     state = {
         clicked: false,
+        changeClr: false,
     }
+    subscribe = (e) => {
+        e.preventDefault();
+        e.disabled = true
+        this.setState({
+            changeClr: true
+        })
+        setTimeout(() => {
+            this.setState({
+                clicked: true,
+                changeClr: false
+            })
+        }, 2000);
+    }
+
+
+
     render() { 
         return (
             <div className="footercard">
@@ -14,8 +31,11 @@ class FooterCard extends Component {
                         <p className="footerCardP">And get the latest articles delivered right to your inbox!</p>
                         <div className="footerforminput">
                             <input aria-label="please insert your email" form="[object Object]" placeholder="youremail@example.com" type="email" className="footerinput" />
-                            <button type="submit" className="footerbutton">Subscribe</button>
+                            <button disabled={this.state.changeClr ? true : false} onClick={(e) => {this.subscribe(e)}} type="submit" className="footerbutton">Subscribe</button>
                         </div>
+                        <h4 style={!this.state.clicked ? {display: "none"} : {display: "block"}} className="promptmsg">Thank you! You will receive your first email shortly 
+                        <span role="img" aria-label="successfully subscribed to news letter">🎉</span>
+                        </h4>
                     </form>
                 </div>
             </div>
