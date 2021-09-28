db6479fa6ff4c057ae9c5dde588b38e51eaf3fd6
Akshay <40710016+akshaykalucha3@users.noreply.github.com>
2021-09-28 11:09:14 +0530

    desktop nav dark theme

--- src/components/Base/NavDesktop.jsx
+++ src/components/Base/NavDesktop.jsx
@@ -1,28 +1,36 @@
 import React, { Component } from 'react'
 import './NavDesktop.css';
 import Mylogo from './mylogo.svg';
-import darkness from './darkness.svg';
+import Darkness from './darkness.svg'
+import Brightness from './brightness.svg';
 import { NavLink } from 'react-router-dom';
+import {connect} from 'react-redux';
+import { setDark } from '../Store/actions';
 
 
 class NavDesktop extends Component {
     state = {  }
+    handleDarkMode = () => {
+        // console.log("making dark")
+        this.props.setIsDark(!this.props.isDark)
+    }
+
     render() { 
         return ( 
-            <div className="parentNav">
+            <div style={ this.props.isDark ? {backgroundColor: "#2b2a2a"} : null } className="parentNav">
                 <div className="desktopnav hfIis">
                     <div className="nameNav">
-                    <img className="Nav__Logo-A" src={Mylogo} alt=""/>
-                    <NavLink to="/">Akcosh</NavLink>
+                    <img style={ this.props.isDark ? {filter: "brightness(0) invert(1)"} : null } className="Nav__Logo-A" src={Mylogo} alt=""/>
+                    <NavLink style={ this.props.isDark ? {color: "white"} : null } to="/">Akcosh</NavLink>
                     </div>
                     <div className="navLinks">
-                        <NavLink activeStyle={{color: "#212121", opacity: "1"}} exact to="/">Home</NavLink>
-                        <NavLink activeStyle={{color: "#212121", opacity: "1"}} to="/about/">About</NavLink>
-                        <NavLink activeStyle={{color: "#212121", opacity: "1"}} to="/blog/">Blog</NavLink>
-                        <NavLink activeStyle={{color: "#212121", opacity: "1"}} to="/software/">Software</NavLink>
-                        <NavLink activeStyle={{color: "#212121", opacity: "1"}} to="/contact/">Contact</NavLink>
-                        <button className="toggleLight">
-                            <img src={darkness} alt="" />
+                        <NavLink style={ this.props.isDark ? {color: "white"} : null }  activeStyle={ this.props.isDark ? {color: "#888686" } : {color: "#212121"}} exact to="/">Home</NavLink>
+                        <NavLink style={ this.props.isDark ? {color: "white"} : null }  activeStyle={ this.props.isDark ? {color: "#888686" } : {color: "#212121"}} to="/about/">About</NavLink>
+                        <NavLink style={ this.props.isDark ? {color: "white"} : null }  activeStyle={ this.props.isDark ? {color: "#888686" } : {color: "#212121"}} to="/blog/">Blog</NavLink>
+                        <NavLink style={ this.props.isDark ? {color: "white"} : null }  activeStyle={ this.props.isDark ? {color: "#888686" } : {color: "#212121"}} to="/software/">Software</NavLink>
+                        <NavLink style={ this.props.isDark ? {color: "white"} : null }  activeStyle={ this.props.isDark ? {color: "#888686" } : {color: "#212121"}} to="/contact/">Contact</NavLink>
+                        <button onClick={this.handleDarkMode} className="toggleLight">
+                            <img src={this.props.isDark ? Brightness : Darkness} alt="" />
                         </button>
                     </div>
                 </div>
@@ -30,5 +38,17 @@ class NavDesktop extends Component {
          );
     }
 }
- 
-export default NavDesktop;
+
+const mapStateToProps = state => {
+    return {
+        isDark: state.WebsiteInterface.isDark
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        setIsDark: (value) => dispatch(setDark(value)),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavDesktop);
