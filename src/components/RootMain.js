492448fa5f7a821c4f2315cb135acecbbbb5ae11
Akshay <40710016+akshaykalucha3@users.noreply.github.com>
2021-09-28 15:08:01 +0530

    desktop nav dark theme

--- src/components/RootMain.js
+++ src/components/RootMain.js
@@ -2,26 +2,16 @@ import React, { Component } from 'react';
 import Main from './Home/Main';
 import { Provider } from 'react-redux';
 import { store } from './Store/store';
-import {connect} from 'react-redux'
 
 
 export class RootMain extends Component {
     render() {
         return(
                 <Provider store={store}>
-                    <div style={ this.props.isDark ? {backgroundColor:"#212121"} : null }>
                     <Main />
-                    </div>
                 </Provider>
             )
     }
 }
 
-const mapStateToProps = state => {
-    return{
-        isDark: state.WebsiteInterface.isDark
-    }
-}
-
-
-export default connect(mapStateToProps)(RootMain);
+export default RootMain
